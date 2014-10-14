f6063be44b4f81c252af0986e02f4a0136a4a12c
cogo79 <migobigo76@gmail.com>
2014-10-14 17:06:42 +0200

    Fixed a runtime error.

--- js/app/view.js
+++ js/app/view.js
@@ -1,24 +1,24 @@
-define(['app/model'], function(model){
+define(['app/model'], function(model) {
 	var c = canvas.getContext('2d');
 
-
-	function render(){
-		
+	function render() {
 		c.clearRect(0, 0, canvas.width, canvas.height);
-		c.beginPath();
 		var prev = model.last();
-		c.moveTo(prev.get('x'), prev.get('y'));
-		model.each(function(curr) {
-			c.lineTo(curr.get('x'), curr.get('y'));
-		});
-		c.closePath();
-		c.fillStyle = "gray";
-		c.fill();
-		c.stroke();
-		model.each(renderVertex);
+		if (prev) {
+			c.beginPath();
+			c.moveTo(prev.get('x'), prev.get('y'));
+			model.each(function(curr) {
+				c.lineTo(curr.get('x'), curr.get('y'));
+			});
+			c.closePath();
+			c.fillStyle = "gray";
+			c.fill();
+			c.stroke();
+			model.each(renderVertex);
+		}
 	}
 
-	function renderVertex(vertex){
+	function renderVertex(vertex) {
 		var x = vertex.get('x'),
 		y = vertex.get('y'),
 		radius = vertex.radius;
